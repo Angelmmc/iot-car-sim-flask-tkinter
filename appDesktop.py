b9dfed8938a9734507b137e6d541ade618e7f91a
Angelmmc <ammcardenas@hotmail.com>
2024-09-22 18:06:45 -0600

    added message when a button is pressed and the button history now displays the last 10 registries previously specified

--- appDesktop.py
+++ appDesktop.py
@@ -1,8 +1,10 @@
+import socket
 import tkinter as tk
 from datetime import date
-import socket
+from tkinter import scrolledtext
+
 import requests
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 BASE_URL = 'http://127.0.0.1:5000/cars'
@@ -21,49 +23,84 @@ def sendInfo(status):
     response = requests.post(BASE_URL, json=custom_car)
     return response.json()
 
+def getInfo(url):
+    try:
+        respuesta = requests.get(url)
+        respuesta.raise_for_status()  # Lanza un error si la solicitud falla
+        datos = respuesta.json()  # Convierte la respuesta JSON en un objeto
+
+        return datos if isinstance(datos, dict) else {'resultados': datos}
+
+    except (requests.exceptions.RequestException, ValueError) as e:
+        print(f"Error: {e}")
+        return None
+
 
 def izquierda90():
-    print("Parar presionado")
+    actualizar_label("Giro a la izquierda de 90°")
     sendInfo("giro_izquierda_90")
 
 
 def avanzar():
-    print("Parar presionado")
+    actualizar_label("Avanzar")
     sendInfo("avanzar")
 
 def derecha90():
-    print("Reiniciar presionado")
+    actualizar_label("Giro a la derecha de 90°")
     sendInfo("giro_derecha_90")
 
 def izquierda():
-    print("Guardar presionado")
+    actualizar_label("Giro a la izquierda")
     sendInfo("giro_izquierda")
 
 def detener():
-    print("Cargar presionado")
+    actualizar_label("Detener")
     sendInfo("detener")
 
 def derecha():
-    print("Salir presionado")
+    actualizar_label("Giro a la derecha")
     sendInfo("giro_derecha")
 
 def izquierda360():
-    print("Ayuda presionado")
+    actualizar_label("Giro a la izquierda de 360°")
     sendInfo("giro_izquierda_360")
 
+
 def historial():
-    print("Configurar presionado")
+    # Hacer la consulta API
+    resultados = getInfo("http://127.0.0.1:5000/cars/latest")
+    nueva_ventana = tk.Toplevel()
+    nueva_ventana.title("Datos de Coches")
+
+    # Crear un área de texto desplazable
+    textarea = scrolledtext.ScrolledText(nueva_ventana, wrap=tk.WORD, width=60, height=20)
+    textarea.pack(padx=10, pady=10)
+
+    # Formatear y añadir los datos al área de texto
+    for coche in resultados['resultados']:
+        textarea.insert(tk.END, f"ID: {coche['id']}\n")
+        textarea.insert(tk.END, f"Nombre: {coche['name']}\n")
+        textarea.insert(tk.END, f"Estado: {coche['status']}\n")
+        textarea.insert(tk.END, f"Fecha: {coche['date']}\n")
+        textarea.insert(tk.END, f"IP del Cliente: {coche['ipClient']}\n")
+        textarea.insert(tk.END, "----------------------\n")
+
+    # Hacer que el área de texto sea de solo lectura
+    textarea.config(state=tk.DISABLED)
 
 def derecha360():
-    print("Actualizar presionado")
+    actualizar_label("Giro a la derecha de 360°")
     sendInfo("giro_derecha_360")
+
+def actualizar_label(nuevo_texto):
+    labelResult.config(text=nuevo_texto)
 
 # Crear la ventana principal
 root = tk.Tk()
 root.title("Controlador del carrito")
 
 # Configurar el tamaño de la ventana
-root.geometry("300x300")
+root.geometry("300x350")
 
 label = tk.Label(root, text="Seleccione una acción:", font=("Arial", 14))
 # Colocar la etiqueta en la parte superior (fila 0)
@@ -86,6 +123,14 @@ for i in range(9):
     button = tk.Button(root, text=textos_botones[i], command=funciones_botones[i], width=10, height=2)
     # Usar grid para colocar los botones en un arreglo de 3 filas x 3 columnas
     button.grid(row=(i//3)+1, column=i%3, padx=10, pady=10)
+
+label = tk.Label(root, text="Acción seleccionada:", font=("Arial", 14))
+    # Colocar la etiqueta en la parte superior (fila 0)
+label.grid(row=4, column=0, columnspan=3, pady=10)
+
+labelResult = tk.Label(root, text="", font=("Arial", 12))
+    # Colocar la etiqueta en la parte superior (fila 0)
+labelResult.grid(row=5, column=0, columnspan=3, pady=10)
 
 # Iniciar el bucle principal de la ventana
 root.mainloop()
